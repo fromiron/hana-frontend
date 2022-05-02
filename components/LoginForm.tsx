617a890807d68fb8f2fcc9871e43c69393e1fb7e
Grayson Lee <artless1112@gmail.com>
2022-05-03 00:57:25 +0900

    State変更機能をlogin componentsからLogin pageに移動し、コンポネントはデザイン関連機能だけもつよう修正

--- components/LoginForm.tsx
+++ components/LoginForm.tsx
@@ -5,31 +5,10 @@ import Button from "@/components/partials/Button";
 import Link from "next/link";
 import Logo from '@/public/logo.svg'
 import Image from 'next/image'
-import {validateEmail, validatePassword} from "@/helpers/validators";
-import {useRecoilState} from "recoil";
-import {loginState} from "@/store/index";
-import {loginStateInterface} from "@/interfaces/index";
+import {loginPropsInterface} from "@/interfaces/index";
 
-export default function LoginForm() {
-    const [state, setState] = useRecoilState<loginStateInterface>(loginState);
 
-    const handleEmailChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-        setState({...state, email: e.target.value});
-    };
-    const handlePasswordChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-        setState({...state, password: e.target.value});
-    };
-
-    const handleValidate = () => {
-        const emailIsValid = validateEmail(state.email);
-        const passwordIsValid = validatePassword(state.password);
-        setState({...state, emailError: !emailIsValid, passwordError: !passwordIsValid});
-    };
-
-    const handleLogin = () => {
-        handleValidate();
-        console.log(state);
-    }
+export default function LoginForm({state, handleEmailChange, handlePasswordChange, handleLogin}: loginPropsInterface) {
     return (
         <div className='bg-white rounded shadow-2xl p-4 m-4 h-fit max-w-lg w-full md:w-2/4'>
             <div className='flex justify-center mt-2 mb-10'>
@@ -50,6 +29,5 @@ export default function LoginForm() {
             </div>
             <Button title={'LOGIN'} onClick={handleLogin}/>
         </div>
-
     )
 }
--- pages/account/login.tsx
+++ pages/account/login.tsx
@@ -2,13 +2,37 @@ import type {NextPage} from 'next'
 import Layout from "@/components/Layout.";
 import React from "react";
 import LoginForm from "@/components/LoginForm";
+import {useRecoilState} from "recoil";
+import {loginStateInterface} from "@/interfaces/index";
+import {loginState} from "@/store/index";
+import {validateEmail, validatePassword} from "@/helpers/validators";
 
 const LoginPage: NextPage = () => {
+    const [state, setState] = useRecoilState<loginStateInterface>(loginState);
+
+    const handleEmailChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setState({...state, email: e.target.value});
+    };
+    const handlePasswordChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setState({...state, password: e.target.value});
+    };
+
+    const handleValidate = () => {
+        const emailIsValid = validateEmail(state.email);
+        const passwordIsValid = validatePassword(state.password);
+        setState({...state, emailError: !emailIsValid, passwordError: !passwordIsValid});
+    };
+
+    const handleLogin = () => {
+        handleValidate();
+        console.log(state);
+    }
+
     return (
         <Layout>
             <div className='relative w-screen h-screen'>
                 <div className="absolute inset-0 bg-black flex justify-center items-center">
-                    <LoginForm/>
+                    <LoginForm state={state} handleEmailChange={handleEmailChange} handlePasswordChange={handlePasswordChange} handleLogin={handleLogin}/>
                 </div>
             </div>
         </Layout>
