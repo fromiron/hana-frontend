6e9445249bc05b828e4e099ae5843078ea7ee7ef
fromiron <artless1112@gmail.com>
2022-05-04 19:07:28 +0900

    IconMenuItemコンポネントにアニメーション適用

--- components/partials/IconMenuItem.tsx
+++ components/partials/IconMenuItem.tsx
@@ -1,11 +1,14 @@
 import React from "react";
 import {IconMenuInterface} from "@/interfaces/index";
+import {motion} from "framer-motion"
 
 export default function IconMenuItem({Icon, title, onClick}: IconMenuInterface) {
     return (
-        <li className='my-4 p-2 cursor-pointer text-mono-200 hover:text-white hover:bg-primary rounded flex' onClick={onClick}>
+        <motion.li whileHover={{scale: 1.02}}
+                   className='my-4 p-2 cursor-pointer text-mono-200 hover:text-white hover:bg-primary rounded flex transition duration-500 ease-in-out'
+                   onClick={onClick}>
             <Icon size={30} className='mr-4'/>{title}
-        </li>
+        </motion.li>
 
     );
 }
--- pages/customers/index.tsx
+++ pages/customers/index.tsx
@@ -5,7 +5,13 @@ import React from "react";
 const CustomersPage: NextPage = () => {
     return (
         <Layout title={'Customers - Rabbit Sitter Hana'} pageTitle={'Customers'}>
-
+            <div>
+                <select>
+                    <option value="">Select a customer</option>
+                    <option value="1">Customer 1</option>
+                    <option value="1">Customer 2</option>
+                </select>
+            </div>
         </Layout>
     )
 }
