701f0fa0ababe8b008afb2959e00008e81a515e5
fromiron <artless1112@gmail.com>
2022-05-17 23:05:56 +0900

    バックエンドのカスタムコントローラ(pet type count)連動テストコード作成

--- pages/cms/overview/index.tsx
+++ pages/cms/overview/index.tsx
@@ -1,16 +1,23 @@
-import type {NextApiRequest, NextPage} from 'next'
+import type { NextPage} from 'next'
 import Layout from "@/components/Layout";
 import React from "react";
-import {parseCookies} from "@/helpers/parseCookies";
 import {GetServerSideProps} from "next";
-import {API_URL, NEXT_API_URL} from "@/config/index";
-import credentialChecker from "@/helpers/credentialChecker";
-import {useQuery} from "react-query";
+import {API_URL} from "@/config/index";
 
-const OverviewPage: NextPage = ({data}: any) => {
+
+const OverviewPage: NextPage<OverviewPageProps> = ({petTypesCountList}) => {
+    console.log(petTypesCountList);
     return (
         <Layout title={'Overview - Rabbit Sitter Hana'} pageTitle={'Overview'}>
             <h1>OverviewPage</h1>
+            {petTypesCountList.map((petTypeCount) => {
+                return (
+                    <div key={petTypeCount.id}>
+                        <h2>{petTypeCount.type}</h2>
+                        <p>{petTypeCount.count}</p>
+                    </div>
+                )
+            })}
         </Layout>
     )
 }
@@ -18,12 +25,31 @@ const OverviewPage: NextPage = ({data}: any) => {
 
 export default OverviewPage
 
+interface PetTypeCountInterface {
+    id: number,
+    count: number
+    type: string
+}
+
+export interface OverviewPageProps {
+    petTypesCountList: PetTypeCountInterface[];
+}
 
 
-export const getServerSideProps: GetServerSideProps = async () => {
+export const getServerSideProps: GetServerSideProps = async (context) => {
+    const petTypeRes = await fetch(`${API_URL}/pet-types/count`, {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${context.req.cookies.token}`
+        }
+    });
+    const data = await petTypeRes.json();
+
+    const props: OverviewPageProps = {
+        petTypesCountList: data
+    }
     return {
-        props: {
-            'data':'test',
-        },
-    };
+        props
+    }
 }
